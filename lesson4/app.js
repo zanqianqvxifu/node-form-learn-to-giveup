b59efd55eac71d6ec097aa4199c983c327466486
zanqianqvxifu <hzwangyan1@corp.netease.com>
2016-07-04 20:43:51 +0800

    @dev lesson4 test undone

--- lesson4/app.js
+++ lesson4/app.js
@@ -1,39 +1,49 @@
 var eventproxy = require('eventproxy'),
 	superagent = require('superagent'),
 	cheerio = require('cheerio'),
-	url = require('url');
+	url = require('url'),
+	express = require('express');
 var cnurl = 'https://cnodejs.org/';
+var app = express();
+app.get('/',function(req,res,next){
 
-superagent.get(cnurl).end(function(err,sres){
-	if(err){
-		return console.log(err,123);
-	}
-	var topUrl = [];
-	var $ = cheerio.load(sres.text);
-	$('#topic_list').find('.topic_title').each(function(key,val){
-		var $ele = $(val);
-		var href = url.resolve(cnurl,$ele.attr('href'));
-		topUrl.push(href);
-	});
-	//console.log(topUrl);
-	var ep =new eventproxy();
-	ep.after('topic_html',topUrl.length,function(topics){
-		topics = topics.map(function(topicPair){
-			var topicUrl = topicPair[0];
-			var topicHtml = topicPair[1];
-			var $ = cheerio.load(topicHtml);
-			return ({
-				title:$('.topic_title').text().trim(),
-				href:topicUrl,
-				comment1: $('.reply_content').eq(0).text().trim()
+	superagent.get(cnurl).end(function(err,sres){
+		if(err){
+			return console.log(err,123);
+		}
+		var topUrl = [];
+		var $ = cheerio.load(sres.text);
+		$('#topic_list').find('.topic_title').each(function(key,val){
+			var $ele = $(val);
+			var href = url.resolve(cnurl,$ele.attr('href'));
+			topUrl.push(href);
+		});
+		//console.log(topUrl);
+		var ep =new eventproxy();
+		ep.after('topic_html',topUrl.length,function(topics){
+			console.log(topics);
+			topics = topics.map(function(topicPair){
+
+				var topicUrl = topicPair[0];
+				var topicHtml = topicPair[1];
+				var $ = cheerio.load(topicHtml);
+				return ({
+					title:$('.topic_title').text().trim(),
+					href:topicUrl,
+					comment1: $('.reply_content').eq(0).text().trim()
+				})
+			})
+			//console.log(topics,1234567789);
+			res.send({topics});
+		});
+		topUrl.forEach(function(topicUrl){
+			superagent.get(topicUrl).end(function(err,sres){
+				console.log('fetch ' + topicUrl + ' successful');
+				ep.emit('topic_html', [topicUrl, sres.text]);
 			})
 		})
-		console.log(topics,1234567789)
-	});
-	topUrl.forEach(function(topicUrl){
-		superagent.get(topicUrl).end(function(err,sres){
-			console.log('fetch ' + topicUrl + ' successful');
-			ep.emit('topic_html', [topicUrl, sres.text]);
-		})
+
 	})
+}).listen(3001,function(){
+	console.log('runs')
 })
